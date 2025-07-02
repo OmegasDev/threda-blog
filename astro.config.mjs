f613fadfa5f448de85f3b4835c588fc96aa5d9ce
OmegasDev <157725661+OmegasDev@users.noreply.github.com>
2025-07-02 22:00:13 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: 'server',
+  adapter: netlify(),
 
   // ✅ ADD THIS LINE 👇
   site: 'http://localhost:4321', // or your future domain
