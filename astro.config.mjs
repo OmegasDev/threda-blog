7ec513b2378035fed17d52df7562c40472649b01
OmegasDev <157725661+OmegasDev@users.noreply.github.com>
2025-07-03 04:43:54 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: 'server',
   adapter: netlify(),
+   adapter: vercel(),
 
   // ✅ ADD THIS LINE 👇
   site: 'http://localhost:4321', // or your future domain
